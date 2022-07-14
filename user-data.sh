7af52e7328896258662697dff1650b070b5cda2d
Austin White <austinw@liatrio.com>
2022-07-14 14:30:56 -0700

    chore: remove redundant sudo commands

--- user-data.sh
+++ user-data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee \
     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
   echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    https://pkg.jenkins.io/debian-stable binary/ | tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
   apt -y update
   apt -y install openjdk-11-jre
@@ -15,10 +15,10 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
     gnupg \
     lsb-release
   mkdir -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt -y update
   apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
