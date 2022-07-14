22b30989aca22bd08dd4e776af4c8523df61fe47
Austin White <austinw@liatrio.com>
2022-07-14 12:45:08 -0700

    chore: update user-data script to install docker and not codeql

---
+++ user-data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -xe
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+  apt -y update
+  apt -y install openjdk-11-jre
+  apt -y install jenkins
+
+  apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+  mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt update
+  apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+  usermod -aG docker jenkins
+
